b725584d9251dda202829eb99452426d8ea5f60e
Eslam Tarek <eslamsulaymaan@gmail.com>
2026-07-15 16:07:35 +0300

    solve race condition using mutual exclusion (Mutex)

---
+++ GolbalMutex/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace GlobalMutex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            using (var mutex = new Mutex(false, "GlobalMutex"))
+            {
+
+                for (int i = 0; i < 100; i++)
+                {
+                    mutex.WaitOne();
+                    try
+                    {
+                        int counter = ReadCounter(filePath);
+                        counter++;
+                        WriteCounter(filePath, counter);
+                    }
+                    finally
+                    {
+                        mutex.ReleaseMutex();
+                    }
+
+                }
+            }
+
+            Console.WriteLine("Process finished.");
+            Console.ReadLine();
+
+
+
+        }
+        static string filePath = "counter.txt";
+        static int ReadCounter(string filePath)
+        {
+            using (var stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite))
+            using (var reader = new StreamReader(stream))
+            {
+                string content = reader.ReadToEnd();
+                return string.IsNullOrEmpty(content) ? 0 : int.Parse(content);
+            }
+        }
+
+        static void WriteCounter(string filePath, int counter)
+        {
+            using (var stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write(counter);
+            }
+        }
+    }
+}
+
--- WebServerSeatBooking/Program.cs
+++ WebServerSeatBooking/Program.cs
@@ -7,44 +7,44 @@ namespace WebServerSeatBooking
         static Lock @lock = new Lock();
         static int avaliableTickeks=10;
         static Queue<string?> requestQueue = new Queue<string?>();
-        //static void Main(string[] args)
-        //{
-        //    Thread thread = new Thread(() => Monitoring());
-        //    thread.Start();
-
-        //    Console.WriteLine("server is running. \r\n type b to book \r\n type b to cancel  type 'exit' to stop \r\n");
-        //    while (true)
-        //    {
-        //        Console.Write("enter your message     ");
-        //        string? input = Console.ReadLine();
-        //        if (input?.ToLower() == "exit")
-        //        {
-        //            break;
-        //        }
-        //        requestQueue.Enqueue(input);
-
-        //    }
-        //}
-
         static void Main(string[] args)
         {
-            Thread[] threads = new Thread[3];
-            threads[0] = new Thread(() => ProcessBookingWithLock("b"));
-            threads[1] = new Thread(() => ProcessBookingWithLock("b"));
-            threads[2] = new Thread(() => ProcessBookingWithLock("b"));
+            Thread thread = new Thread(() => Monitoring());
+            thread.Start();
 
-            foreach (var item in threads)
+            Console.WriteLine("server is running. \r\n type b to book \r\n type b to cancel  type 'exit' to stop \r\n");
+            while (true)
             {
-                item.Start();
-            }
-            foreach (var item in threads)
-            {
-                item.Join();
-            }
+                Console.Write("enter your message     ");
+                string? input = Console.ReadLine();
+                if (input?.ToLower() == "exit")
+                {
+                    break;
+                }
+                requestQueue.Enqueue(input);
 
-            Console.WriteLine(avaliableTickeks);
-
+            }
         }
+
+        //static void Main(string[] args)
+        //{
+        //    Thread[] threads = new Thread[3];
+        //    threads[0] = new Thread(() => ProcessBookingWithLock("b"));
+        //    threads[1] = new Thread(() => ProcessBookingWithLock("b"));
+        //    threads[2] = new Thread(() => ProcessBookingWithLock("b"));
+
+        //    foreach (var item in threads)
+        //    {
+        //        item.Start();
+        //    }
+        //    foreach (var item in threads)
+        //    {
+        //        item.Join();
+        //    }
+
+        //    Console.WriteLine(avaliableTickeks);
+
+        //}
         static void Monitoring()
         {
             while (true)
@@ -59,33 +59,74 @@ namespace WebServerSeatBooking
 
             }
         }
+        //static void ProcessBooking(string? input)
+        //{
+        //    Thread.Sleep(2000);
+        //    lock (@lock)
+        //    {
+        //        if (input == "b")
+        //        {
+        //            if (avaliableTickeks > 0)
+        //            {
+        //                avaliableTickeks--;
+        //                Console.WriteLine();
+        //                Console.WriteLine($"you have book a seat and still there are {avaliableTickeks} seats avaliable");
+        //            }
+        //        }
+
+        //        else if (input == "c")
+        //        {
+
+        //            if (avaliableTickeks < 10)
+        //            {
+        //                avaliableTickeks++;
+        //                Console.WriteLine();
+        //                Console.WriteLine($"you have cancelled a seat and there are {avaliableTickeks} seats avaliable");
+        //            }
+        //        }
+
+        //    }
+        //}
         static void ProcessBooking(string? input)
         {
-            Thread.Sleep(2000);
-            lock (@lock)
+            if (Monitor.TryEnter(@lock, 2000))
             {
-                if (input == "b")
+                try
                 {
-                    if (avaliableTickeks > 0)
+                    Thread.Sleep(10000);
                     {
-                        avaliableTickeks--;
-                        Console.WriteLine();
-                        Console.WriteLine($"you have book a seat and still there are {avaliableTickeks} seats avaliable");
+                        if (input == "b")
+                        {
+                            if (avaliableTickeks > 0)
+                            {
+                                avaliableTickeks--;
+                                Console.WriteLine();
+                                Console.WriteLine($"you have book a seat and still there are {avaliableTickeks} seats avaliable");
+                            }
+                        }
+
+                        else if (input == "c")
+                        {
+
+                            if (avaliableTickeks < 10)
+                            {
+                                avaliableTickeks++;
+                                Console.WriteLine();
+                                Console.WriteLine($"you have cancelled a seat and there are {avaliableTickeks} seats avaliable");
+                            }
+                        }
                     }
                 }
-
-                else if (input == "c")
+                finally
                 {
-
-                    if (avaliableTickeks < 10)
-                    {
-                        avaliableTickeks++;
-                        Console.WriteLine();
-                        Console.WriteLine($"you have cancelled a seat and there are {avaliableTickeks} seats avaliable");
-                    }
+                    Monitor.Exit(@lock);
                 }
-
             }
+            else
+            {
+                Console.WriteLine("the Server is busy right now. try later");
+            }
+            
         }
         static void ProcessBookingWithLock(string? input)
         {
