be2e046cf4842db8f94da02af9277b18b61337d4
Eslam Tarek <eslamsulaymaan@gmail.com>
2026-07-15 18:05:42 +0300

    use semaphore to limit the number of concurrent threads.

---
+++ WebServerUsingSemaphore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WebServerUsingSemaphore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
