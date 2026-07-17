82f6dfaef5c130cb3e23af373162325663a64625
Eslam Tarek <eslamsulaymaan@gmail.com>
2026-07-17 16:09:21 +0300

    add manual reset event

---
+++ ManualResetEvent/Program.cs
@@ -0,0 +1,24 @@
+﻿using ManualResetEvent manualResetEvent = new ManualResetEvent(false);
+
+Console.WriteLine("press enter to release all threads");
+
+for (int i = 1; i <= 3; i++)
+{
+    Thread thread = new Thread(work);
+    thread.Name = $"Worker {i}";
+    thread.Start();
+}
+
+Console.ReadLine();
+
+manualResetEvent.Set();
+
+
+void work()
+{
+    Console.WriteLine($"{Thread.CurrentThread.Name} is waiting for signal");
+    manualResetEvent.WaitOne();
+    Thread.Sleep(2000);
+    Console.WriteLine($"{Thread.CurrentThread.Name} is released");
+}
+
