13f518ea0884d3c82b4f57e84e7889a85d3394e5
Eslam Tarek <eslamsulaymaan@gmail.com>
2026-07-17 16:00:33 +0300

    add auto reset event

---
+++ SignlingWithAutoResetEvent/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SignalingWithAutoResetEvent
+{
+    internal class Program
+    {
+        static AutoResetEvent autoResetEvent = new(false);
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                Thread thread = new Thread(Worker);
+                thread.Name = $"Worker {i + 1} ";
+                thread.Start();
+            }
+            string? input;
+            Console.WriteLine("server is running enter go to proceed");
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "go")
+                {
+                    autoResetEvent.Set();
+                }
+            }
+        }
+        static void Worker()
+        {
+            while (true)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} is waiting for signal to be set");
+
+                autoResetEvent.WaitOne();
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} is proceeded");
+                Thread.Sleep(2000);
+
+            }
+        }
+    }
+
+}
--- WebServerUsingSemaphore/Program.cs
+++ WebServerUsingSemaphore/Program.cs
@@ -1,10 +1,63 @@
 ﻿namespace WebServerUsingSemaphore
+
 {
     internal class Program
     {
+        static Queue<string?> requestQueue = new Queue<string?>();
+        static SemaphoreSlim semaphore = new SemaphoreSlim(initialCount: 3, maxCount: 3);
+        static Lock Lock = new Lock();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Thread thread = new Thread(() => Monitoring());
+            thread.Start();
+
+            Console.WriteLine("server is running. type  'exit' to stop");
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (input?.ToLower() == "exit")
+                {
+                    break;
+                }
+                lock (Lock)
+                {
+                    requestQueue.Enqueue(input);
+                }
+
+            }
+        }
+        static void Monitoring()
+        {
+            while (true)
+            {
+                if (requestQueue.Count > 0)
+                {
+                    string? input;
+                    lock (Lock)
+                    {
+                        input = requestQueue.Dequeue();
+                    }
+                    semaphore.Wait();
+                    Thread thread = new Thread(() => ProcessInput(input));
+                    thread.Start();
+                }
+                Thread.Sleep(100);
+
+            }
+        }
+        static void ProcessInput(string? input)
+        {
+            try
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine($"\nyour input is {input}\n");
+            }
+            finally
+            {
+                var pervCount = semaphore.Release();
+                Console.WriteLine($"thead with id {Thread.CurrentThread.ManagedThreadId} and the previous count ={pervCount} ");
+            }
+
         }
     }
 }
